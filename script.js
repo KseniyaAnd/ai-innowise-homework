bb0beef4b0764a52396af487d807b75215283241
KseniyaAnd <kseniya.andrusenko@gmail.com>
2025-06-03 12:21:53 +0200

    init: done second task

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.getElementById('menuBtn');
+    const menu = document.getElementById('menu');
+
+    if (menuBtn && menu) {
+        menuBtn.addEventListener('click', () => {
+            menu.classList.toggle('open');
+        });
+    }
+});
